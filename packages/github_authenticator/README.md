a0908606f3b46a530837f838d09d5092f3f77a22
Andrew Brogdon <brogdon@gmail.com>
2024-12-11 15:26:45 -0800

    Adding server

---
+++ packages/github_authenticator/README.md
@@ -0,0 +1,19 @@
+# GitHub Authentictor
+
+A simple server app designed to execute on Cloud Run and provide
+OAuth tokens.
+
+It's based on the Code Sharing sample found in the 
+[Flutter samples](https://github.com/flutter/samples).
+
+# Running the sample
+
+## Running with the Dart SDK
+
+You can run the example with the [Dart SDK](https://dart.dev/get-dart)
+like this:
+
+```
+$ dart run bin/server.dart
+Server listening on port 8080
+```
---
+++ packages/github_authenticator/bin/server.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+int count = 0;
+
+// Configure routes.
+final _router = Router()
+  ..post('/version', _versionHandler)
+  ..get('/', _errorHandler);
+
+Future<Response> _versionHandler(Request request) async {
+  return Response.ok('1.0.0');
+}
+
+Response _errorHandler(Request request) => Response.badRequest();
+
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
